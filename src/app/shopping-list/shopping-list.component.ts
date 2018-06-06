6a6b75b91b426b4db47e158528c278bdc8e273ab
loveleen <loveleen.nancy@gmail.com>
2018-06-07 02:26:31 +0530

    Final Data Binding

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,14 +9,16 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Mango',2),
-    new Ingredient('Apple',4),
+    new Ingredient('Mango', 4),
+    new Ingredient('Apple', 6),
   ];
-  
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
