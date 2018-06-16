b80e811fd0c417f050912c93a074677ed775f1e8
loveleen <loveleen.nancy@gmail.com>
2018-06-17 00:25:13 +0530

    Final Routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -16,7 +16,7 @@ export class RecipeService {
         new Ingredient('CornFlour',3)
     ]),
         new Recipe('Noodles',
-        'Test','https://i0.wp.com/media.hungryforever.com/wp-content/uploads/2015/04/Featured-image-masala-dosa-recipe.jpg?ssl=1?w=1269&strip=all&quality=80',
+        'Test','https://uz71pyzpz0-flywheel.netdna-ssl.com/wp-content/uploads/2018/02/garlic-noodles-61-700x680.jpg',
     [
         new Ingredient('Noodles',1),
         new Ingredient('SOya',4)
@@ -27,6 +27,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+      return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
